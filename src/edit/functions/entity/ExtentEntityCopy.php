b5319fb139ddd3980d7928b38e80b27ff4de90dd
Nerahikada <hkshirorom@yahoo.co.jp>
2019-07-15 23:07:44 +0900

    Need more research

--- src/edit/functions/entity/ExtentEntityCopy.php
+++ src/edit/functions/entity/ExtentEntityCopy.php
@@ -44,8 +44,9 @@ class ExtentEntityCopy implements EntityFunction{
 	public function apply(Entity $entity) : bool{
 		$location = new Location($this->destination, new Vector($entity->x, $entity->y, $entity->z), $entity->getYaw(), $entity->getPitch());
 		$pivot = $this->from->round()->add(0.5, 0.5, 0.5);
+
 		$newPosition = $this->transform->apply($location->toVector()->subtract($pivot));
-		$newLocation = new Location($this->destination, $newPosition, $entity->getYaw(), $entity->getPitch());
+		$newLocation = new Location($this->destination, $newPosition->add($this->to->round()->add(0.5, 0.5, 0.5)), $entity->getYaw(), $entity->getPitch());
 
 		$entity = $this->transformNbtData($entity);
 
